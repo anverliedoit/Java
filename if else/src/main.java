60aa4f1b4cc9d2a9a9c2bf656b81f6fe83efbf01
gglifekillmetot <lieverandiver@gmail.com>
2018-03-14 15:30:50 +0800

    first commit

---
+++ if else/src/main.java
@@ -0,0 +1,15 @@
+public class main {
+
+    public  static  void main(String args[]){
+
+        int test = 6;
+
+        if (test == 9){
+
+            System.out.println("Yes");
+        }else{
+            System.out.print("this is elese");
+        }
+
+    }
+}

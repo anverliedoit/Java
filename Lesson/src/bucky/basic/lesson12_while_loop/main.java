f0859f51594005599a69b108bc5353c6b9aa143a
gglifekillmetot <lieverandiver@gmail.com>
2018-03-15 10:55:54 +0800

    Java Programming Tutorial - 13 - While Loop

---
+++ Lesson/src/bucky/basic/lesson12_while_loop/main.java
@@ -0,0 +1,14 @@
+package bucky.basic.lesson12_while_loop;
+
+public class main {
+    public static void main(String args[]){
+        int counter = 0;
+
+        while(counter < 10){
+            System.out.println(counter);
+            counter++;
+        }
+
+    }
+}
+

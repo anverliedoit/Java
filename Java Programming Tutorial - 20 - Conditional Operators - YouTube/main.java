6cb574e009b3570e28513f9ba619f6cd12af1a8e
gglifekillmetot <lieverandiver@gmail.com>
2018-04-11 21:27:31 +0800

    Conditional Operators

---
+++ Java Programming Tutorial - 20 - Conditional Operators - YouTube/main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class main {
+
+    public static void main(String args[]){
+
+        int age = 60;
+        System.out.println(age > 50 ? "Your are old" : "You are young");
+
+    }
+}

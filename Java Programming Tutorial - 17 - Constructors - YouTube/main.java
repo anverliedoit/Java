c8e1ca5dbb560ca3131a92f3cbfa0ffa7c0020cf
gglifekillmetot <lieverandiver@gmail.com>
2018-04-11 21:15:27 +0800

    Contrustion

---
+++ Java Programming Tutorial - 17 - Constructors - YouTube/main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class main {
+
+    public static void main(String args[]){
+
+         tuna tunaObject = new tuna("Janine");
+        tuna tunaObject2 = new tuna("Nicole");
+         tunaObject.saying();
+        tunaObject2.saying();
+
+    }
+}
